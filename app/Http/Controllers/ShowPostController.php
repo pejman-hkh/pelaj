cf08e1ca20d329bfecf68aa6e065cfb37b055974
pejman <pejman.hkh@gmail.com>
2023-06-27 13:27:22 +0330

    e

--- app/Http/Controllers/ShowPostController.php
+++ app/Http/Controllers/ShowPostController.php
@@ -12,18 +12,25 @@ use Spatie\RouteAttributes\Attributes\Middleware;
 #[Middleware([\App\Http\Middleware\ShareViewSite::class])]
 class ShowPostController extends Controller
 {
+
+    #[Get('post/{id}')]
+    function show( Request $request, $id ) {
+        $post = Post::where('id', $id )->first();
+        if( @$post->id ) {
+            return view('Site::post', ['post' => $post ]);
+        }
+        abort(404);
+    }
+        
     #[Get('/{any}')]
     function index( Request $request ) {
         $path = substr( $request->getPathInfo(), 1 );
-
-        if( substr( $path, 0, 4 ) == 'post' )
-            $post = Post::where('id', (int)substr( $path, 5 ) )->first();
-        else
-            $post = Post::where('url', $path)->first();
+        $post = Post::where('url', $path)->first();
         
         if( @$post->id ) {
             return view('Site::post', ['post' => $post ]);
         }
         abort(404);
     }
+
 }
