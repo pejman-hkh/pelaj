53269ad8db8b7d1ce020b7957a499571593933fd
pejman <pejman.hkh@gmail.com>
2023-06-30 23:33:13 +0330

    e

--- app/views/theme/default/partials/header.blade.php
+++ app/views/theme/default/partials/header.blade.php
@@ -16,49 +16,108 @@
 
 <body class="bg-white font-family-karla">
 
-    <!-- Top Bar Nav -->
-    <nav class="w-full py-4 bg-blue-800 shadow">
-        <div class="w-full container mx-auto flex flex-wrap items-center justify-between">
 
-            <nav>
-                <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+    <nav x-data="{ open: false }" class="bg-blue-800 border-b border-gray-100">
+        <!-- Primary Navigation Menu -->
+        <div class="mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('dashboard') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        </a>
+                    </div>
 
-                    @if ( \Illuminate\Support\Facades\Auth::check() )
-                       <li> <a class="px-4" href="{{ route('dashboard') }}"> {{ __('Dashboard') }}</a> </li>
-                    @else
-                        <li> <a class="px-4" href="{{ route('login') }}"> {{ __('Login') }}</a> </li>
-                        <li> <a class="px-4" href="{{ route('register') }}"> {{ __('Register') }}</a> </li>
-                    @endif
-                    @foreach ( (array)@$menus[1] as $menu )
-                        <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ $menu->link }}">{{ $menu->title }}</a></li>
-                    @endforeach
+                    <!-- Navigation Links -->
+                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                         <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
+
+                            @if ( Auth::check() )
+                               <li> <a class="px-4" href="{{ route('dashboard') }}"> {{ __('Dashboard') }}</a> </li>
+                            @else
+                                <li> <a class="px-4" href="{{ route('login') }}"> {{ __('Login') }}</a> </li>
+                                <li> <a class="px-4" href="{{ route('register') }}"> {{ __('Register') }}</a> </li>
+                            @endif
+                            @foreach ( (array)@$menus[1] as $menu )
+                                <li><a class="hover:text-gray-200 hover:underline px-4" href="{{ $menu->link }}">{{ $menu->title }}</a></li>
+                            @endforeach
 
 
-                </ul>
-            </nav>
+                        </ul>
+                    </div>
+                </div>
 
-            <div class="flex items-center text-lg no-underline text-white pr-6">
-                <a class="" href="#">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-linkedin"></i>
-                </a>
+
+                <!-- Hamburger -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
 
+        <!-- Responsive Navigation Menu -->
+        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                         <ul class="font-bold text-sm text-white uppercase no-underline">
+
+                            @if ( ! Auth::check() )
+                                <li class="p-2 px-4"> <a href="{{ route('login') }}"> {{ __('Login') }}</a> </li>
+                                <li class="p-2 px-4"> <a href="{{ route('register') }}"> {{ __('Register') }}</a> </li>
+                            @endif
+                            @foreach ( (array)@$menus[1] as $menu )
+                                <li class="p-2 px-4"><a class="hover:text-gray-200 hover:underline" href="{{ $menu->link }}">{{ $menu->title }}</a></li>
+                            @endforeach
+
+
+                        </ul>
+            </div>
+
+            @if( Auth::check() )
+            <div class="pt-2 pb-3 space-y-1">
+                <x-responsive-nav-link  class="text-white" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+            </div>
+
+            <!-- Responsive Settings Options -->
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="px-4">
+                    <div class="font-medium text-base text-white">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-sm text-white">{{ Auth::user()->email }}</div>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <x-responsive-nav-link :href="route('profile.edit')" class="text-white">
+                        {{ __('Profile') }}
+                    </x-responsive-nav-link>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-responsive-nav-link  class="text-white" :href="route('logout')"
+                                onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-responsive-nav-link>
+                    </form>
+                </div>
+            </div>
+            @endif
+        </div>
     </nav>
+
 
     <!-- Text Header -->
     <header class="w-full container mx-auto">
         <div class="flex flex-col items-center py-12">
-            <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
+            <a class="font-bold p-4 text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
                 {{ @$configs->mainTitle }}
             </a>
             <p class="text-lg text-gray-600">
