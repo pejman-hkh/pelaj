1b57fdd890c37ade391b7d94fb917b146d80f3f6
pejman <pejman.hkh@gmail.com>
2023-07-01 17:40:36 +0330

    e

--- modules/Shop/App/Models/Product.php
+++ modules/Shop/App/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public static $listExceptColumns = [
+        'summery',
+    ];
+
+    public static $formExceptColumns = [
+        'user_id',
+    ];
+
+    public static $formEditorColumns = [
+        'summery',
+    ];
+
 }
