74995ab3cd233fa9311f8a9ddd95e18351311f69
pejman <pejman.hkh@gmail.com>
2023-07-07 17:13:12 +0330

    e

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ cp .env.sqlite .env;
 pwd=$(pwd)
 sed -i 's#\$pwd#'$pwd'#' .env;
 chmod -R 0777 storage
-touch storage/app/laravel.sqlite
+touch database/laravel.sqlite
 composer install
 npm i
 npm run build
 php artisan key:generate
-chmod 0777 storage/app/laravel.sqlite
+chmod 0777 database/laravel.sqlite
 php artisan migrate:refresh --seed
